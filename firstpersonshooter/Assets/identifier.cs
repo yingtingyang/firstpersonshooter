e4fb61ef56516fc27375263ed4ffa0137d1ed361
yingtingyang <yangy389@newschool.edu>
2016-10-13 18:33:21 -0400

    initial

---
+++ firstpersonshooter/Assets/identifier.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class identifier : MonoBehaviour {
+    Image crosshair;
+
+	// Use this for initialization
+	void Start () {
+        crosshair = GameObject.Find("crosshair").GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        crosshair.color = Color.red;
+        RaycastHit nfo;
+        if (Physics.Raycast(transform.position,transform.forward,out nfo) == true)
+        {
+            if(nfo.tranform.CompareTag("Enemy")== true)
+            {
+                crosshair.color = Color.yellow;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    nfo.transform.GetComponent<addparticle>().countscore();
+                }
+            }
+        }
+	
+	}
+}
